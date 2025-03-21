cca8953f61c73858dde102c3a9db8619a89a5e3f
Slobodan <kircanskis@gmail.com>
2025-03-21 23:36:31 +0100

    Create a loop to make a 16x16 grid of div elements
    Also added hover effect when a mouse goes over the single element

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
 const container = document.querySelector("#container");
-const square = document.createElement("div");
-square.classList.add("square")
-container.appendChild(square);
+let i = 0;
+for (let i = 0; i < 16 ** 2; i++) {
+    const square = document.createElement("div");
+    square.classList.add("square")
+    container.appendChild(square);
+    console.log(i);
+} 
+
+const hoverOver = document.querySelectorAll(".square");
+
+hoverOver.forEach(element => {
+    element.addEventListener("mouseover", () => {
+        element.classList.add("color-change");
+    })
+});
+
+
+
+
+
