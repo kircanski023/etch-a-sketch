891be7bcbdf8b81efd2270b7df55ea16ad66aca0
Slobodan <kircanskis@gmail.com>
2025-03-22 23:45:36 +0100

    Add a button to resize grid

--- script.js
+++ script.js
@@ -1,20 +1,47 @@
-const container = document.querySelector("#container");
-let i = 0;
-for (let i = 0; i < 16 ** 2; i++) {
-    const square = document.createElement("div");
-    square.classList.add("square")
-    container.appendChild(square);
-    console.log(i);
-} 
+const body = document.querySelector("body");
+let container = document.querySelector("#container");
+let gridNumber = 16;
 
-const hoverOver = document.querySelectorAll(".square");
+function createCanvas(){
+    for (let i = 0; i < gridNumber ** 2; i++) {
+        const square = document.createElement("div");
+        square.classList.add("square")
+        container.appendChild(square);
+    } 
+}
 
-hoverOver.forEach(element => {
-    element.addEventListener("mouseover", () => {
-        element.classList.add("color-change");
-    })
-});
+createCanvas(container);
 
+function colorSquares(){
+    const hoverOver = document.querySelectorAll(".square");
+    hoverOver.forEach(element => {
+        element.addEventListener("mouseover", () => {
+            element.classList.add("color-change");
+        })
+    });
+}
+colorSquares();
+/////////////////////////////////////////
+const buttonInputGrid = document.querySelector("#gridInput");
+
+buttonInputGrid.addEventListener("click", () => {
+const gridInput = prompt("Resize grid to: ");
+gridNumber = gridInput;
+let gridSize = Number((gridInput * 20) + (gridInput * 4));
+    if(gridInput <= 100){
+        container.remove();
+        container = document.createElement("div");
+        container.setAttribute("style",
+             `height: ${gridSize}px;
+              width: ${gridSize}px;
+              display: flex;
+              flex-wrap: wrap;
+              border: 2px solid bisque;`);
+        body.appendChild(container);
+        createCanvas(container);
+        colorSquares();
+    }
+})
 
 
 
